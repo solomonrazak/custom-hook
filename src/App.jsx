73e43a76a83db3becd8d31fa59c9b4349238fda2
solomonrazak <solomonrazak99@gmail.com>
2024-06-26 11:25:41 -0700

    first custom hook completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,11 @@
-
-import React, {useEffect, useState} from 'react';
 import './App.css'
+import { useRandomJoke } from './useRandomJoke';
 
 function App() {
 
-  const [joke, setJoke] = useState('')
+  const joke = useRandomJoke();
 
-  useEffect(() => {
-    const fetchJoke = async() => {
-      try {
-        const response = await fetch('http://api.icndb.com/jokes/random?firstName=Solomon&lastName=Razak')
-        if(!response.ok){
-          throw new Error(`this is the error: ${response.status}`)
-        }
-        let data = await response.json();
-        setJoke(data.value.joke)
-
-      }
-      catch (error){
-        console.log(error)
-
-      }
-    }
-    fetchJoke();
-
-  }, [])
+ 
   
 
   return (
--- src/useRandomJoke.jsx
+++ src/useRandomJoke.jsx
@@ -1,11 +1,29 @@
 import React, {useState, useEffect} from 'react'
 
-const useRandomJoke = () => {
+export const useRandomJoke = () => {
 
-    const [joke, setJoke] = useState('')
-  return (
-    <div>useRandomJoke</div>
-  )
-}
+    const [joke, setJoke] = useState('');
 
-export default useRandomJoke;
+    useEffect(() => {
+        const fetchJoke = async() => {
+          try {
+            const response = await fetch('http://api.icndb.com/jokes/random?firstName=Solomon&lastName=Razak')
+            if(!response.ok){
+              throw new Error(`this is the error: ${response.status}`)
+            }
+            let data = await response.json();
+            setJoke(data.value.joke)
+    
+          }
+          catch (error){
+            console.log(error)
+    
+          }
+        }
+        fetchJoke();
+    
+      }, [])
+
+      return joke;
+
+    }
