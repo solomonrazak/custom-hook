668d57e0100a657c393191b148e85a6b4400f6fe
solomonrazak <solomonrazak99@gmail.com>
2024-06-26 11:00:51 -0700

    custom hook component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,39 @@
 
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css'
 
 function App() {
+
+  const [joke, setJoke] = useState('')
+
+  useEffect(() => {
+    const fetchJoke = async() => {
+      try {
+        const response = await fetch('http://api.icndb.com/jokes/random?firstName=Solomon&lastName=Razak')
+        if(!response.ok){
+          throw new Error(`this is the error: ${response.status}`)
+        }
+        let data = await response.json();
+        setJoke(data.value.joke)
+
+      }
+      catch (error){
+        console.log(error)
+
+      }
+    }
+    fetchJoke();
+
+  }, [])
   
 
   return (
     
    <div>
     <center>
-      <h1>The Joke Generatot</h1>
+      <h1>The Joke Generator</h1>
       <button>Generate Joke</button>
+      <p>{joke}</p>
     </center>
    </div>
   )
---
+++ src/useRandomJoke.jsx
@@ -0,0 +1,11 @@
+import React, {useState, useEffect} from 'react'
+
+const useRandomJoke = () => {
+
+    const [joke, setJoke] = useState('')
+  return (
+    <div>useRandomJoke</div>
+  )
+}
+
+export default useRandomJoke;
