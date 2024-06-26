93d529b2cd7ccc6466f2e51a53787d7fc686a020
solomonrazak <solomonrazak99@gmail.com>
2024-06-26 10:20:18 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import './App.css'
+
+function App() {
+  
+
+  return (
+    
+   <div>
+    <center>
+      <h1>The Joke Generatot</h1>
+      <button>Generate Joke</button>
+    </center>
+   </div>
+  )
+}
+
+export default App
